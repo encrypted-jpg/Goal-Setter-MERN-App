d1b14d67470841a82bf1516eff399882f447cc53
encrypted_jpg <vallakatlaaditya@gmail.com>
2022-12-13 22:16:06 +0530

    Completed Frontend

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+function Spinner() {
+    return (
+        <div className="loadingSpinnerContainer">
+            <div className="loadingSpiner">
+
+            </div>
+      </div>
+  )
+}
+
+export default Spinner
