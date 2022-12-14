1f3726914b5d7ede91f0c098f7fdfdeb4704c001
encrypted_jpg <vallakatlaaditya@gmail.com>
2022-12-14 16:13:06 +0530

    Update API_URL

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/goals/';
+const API_URL = 'https://goal-setter-api-345d.onrender.com/api/goals/';
 
 // Create new goal
 const createGoal = async (goalData, token) => {
