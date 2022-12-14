5df40ceb62e18c914b266e0e01537edfa515f277
encrypted_jpg <vallakatlaaditya@gmail.com>
2022-12-14 17:23:00 +0530

    Enabled CORS

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://goal-setter-static.onrender.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header("Access-Control-Allow-Origin", "https://goal-setter-static.onrender.com"); // update to match the domain you will make the request from
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
   });
 
